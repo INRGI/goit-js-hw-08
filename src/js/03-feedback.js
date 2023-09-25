af4ff0c579d44b295447215b9217794a41087748
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-09-25 07:04:00 +0200

    feedback task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+
+const selectForm = document.querySelector('.feedback-form');
+const email = selectForm.querySelector('input');
+const message = selectForm.querySelector('textarea');
+
+
+selectForm.addEventListener("input", throttle(saveMessage, 500));
+selectForm.addEventListener('submit', onSubmit);
+
+let dataUserInfo = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+onReload()
+
+function saveMessage(event) {
+    dataUserInfo = { email: email.value, message: message.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataUserInfo));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(dataUserInfo);
+
+    localStorage.removeItem('feedback-form-state');
+    selectForm.reset();
+    dataUserInfo = {};
+}
+
+function onReload() {
+    if (dataUserInfo) {
+        email.value = dataUserInfo.email || '';
+        message.value = dataUserInfo.message || '';
+    }
+}
+
 
